6b81afeae63f00ff4b08aed58aa6a01aeb8a0791
Axel Espinosa <axeldavid45@gmail.com>
2021-02-24 20:12:05 -0600

    style(models): user model style transformed

--- models/user.js
+++ models/user.js
@@ -1,15 +1,44 @@
 const { model, Schema } = require('mongoose')
 
 const definition = {
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  fullname: { type: String, required: false },
-  weight: { type: Number, required: false },
-  age: { type: Number, min: 8, max: 100, required: false },
-  imc: { type: Number, required: false },
-  activityLevel: { type: Number, required: false },
-  createdAt: { type: Date, default: Date.now },
-  lastLogin: { type: Date, required: false }
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  fullname: {
+    type: String,
+    required: false
+  },
+  weight: {
+    type: Number,
+    required: false
+  },
+  age: {
+    type: Number,
+    min: 8,
+    max: 100,
+    required: false
+  },
+  imc: {
+    type: Number,
+    required: false
+  },
+  activityLevel: {
+    type: Number,
+    required: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  lastLogin: {
+    type: Date,
+    required: false
+  }
 }
 
 const user = new Schema(definition)
