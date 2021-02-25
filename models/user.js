408b959094482e4515003f9928601390da2b1743
Axel Espinosa <axeldavid45@gmail.com>
2021-02-24 20:00:02 -0600

    feat(models): Added user model

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const { model, Schema } = require('mongoose')
+
+const definition = {
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  fullname: { type: String, required: false },
+  weight: { type: Number, required: false },
+  age: { type: Number, min: 8, max: 100, required: false },
+  imc: { type: Number, required: false },
+  activityLevel: { type: Number, required: false },
+  createdAt: { type: Date, default: Date.now },
+  lastLogin: { type: Date, required: false }
+}
+
+const user = new Schema(definition)
+
+module.exports = model('User', user)
